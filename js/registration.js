298222b03a43c0e01c609f4348fa1dd8e6e8a7d0
igorf <igorf@igorf-desktop.(none)>
2014-12-16 12:26:22 +0200

    Next Step. registration.js - send data by post to register.php

--- js/registration.js
+++ js/registration.js
@@ -9,7 +9,28 @@ var country = $("#country").val();
 var city = $("#city").val();
 var pcode = $("#pcode").val();
 if (fname == '' || sname == '' || email == '' || age == '' || sex == '' || country == '' || city == '' || pcode == ''  ) {
-	alert("Please fill all fields...!!!!!!");
-} 
+	alert("NB! Please fill all fields!");
+} else 
+			if ((fname.length) < 2) {
+				alert("Name should atleast 2 character in length...!");
+			} else if ((sname.length) < 2) {
+				alert("Surname should atleast 2 character in length...!");
+			} else {
+			$.post("register.php", {
+				fname1: fname,
+				sname1: sname,
+				email1: email,
+				age1: age,
+				sex1: sex,
+				country1: country,
+				city1: city,
+				pcode1: pcode
+				}, function(data) {
+					if (data == 'You have Successfully Registered.....') {
+						$("form")[0].reset();
+					}
+					alert(data);
+				});
+			}
 });
 });

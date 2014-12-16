006c980262e3527065695f123b8607f3322c8e36
igorf <igorf@igorf-desktop.(none)>
2014-12-16 13:40:19 +0200

    Next step. Reload captcha.

--- js/registration.js
+++ js/registration.js
@@ -1,36 +1,43 @@
 $(document).ready(function() {
-$("#register").click(function() {
-var fname = $("#fname").val();
-var sname = $("#sname").val();
-var email = $("#email").val();
-var age = $("#age").val();
-var sex = $("#sex").val();
-var country = $("#country").val();
-var city = $("#city").val();
-var pcode = $("#pcode").val();
-if (fname == '' || sname == '' || email == '' || age == '' || sex == '' || country == '' || city == '' || pcode == ''  ) {
-	alert("NB! Please fill all fields!");
-} else 
-			if ((fname.length) < 2) {
-				alert("Name should atleast 2 character in length...!");
-			} else if ((sname.length) < 2) {
-				alert("Surname should atleast 2 character in length...!");
-			} else {
-			$.post("register.php", {
-				fname1: fname,
-				sname1: sname,
-				email1: email,
-				age1: age,
-				sex1: sex,
-				country1: country,
-				city1: city,
-				pcode1: pcode
-				}, function(data) {
-					if (data == 'You have Successfully Registered.....') {
-						$("form")[0].reset();
-					}
-					alert(data);
-				});
-			}
-});
+        // Change CAPTCHA on each click or on refreshing page.
+        $("#reload").click(function() {
+                        $("#img").remove();
+                        location.reload();
+                        $('<img id="img" src="captcha.php" />').appendTo("#imgdiv");
+                });
+
+	$("#register").click(function() {
+	var fname = $("#fname").val();
+	var sname = $("#sname").val();
+	var email = $("#email").val();
+	var age = $("#age").val();
+	var sex = $("#sex").val();
+	var country = $("#country").val();
+	var city = $("#city").val();
+	var pcode = $("#pcode").val();
+	if (fname == '' || sname == '' || email == '' || age == '' || sex == '' || country == '' || city == '' || pcode == ''  ) {
+		alert("NB! Please fill all fields!");
+	} else 
+				if ((fname.length) < 2) {
+					alert("Name should atleast 2 character in length...!");
+				} else if ((sname.length) < 2) {
+					alert("Surname should atleast 2 character in length...!");
+				} else {
+				$.post("register.php", {
+					fname1: fname,
+					sname1: sname,
+					email1: email,
+					age1: age,
+					sex1: sex,
+					country1: country,
+					city1: city,
+					pcode1: pcode
+					}, function(data) {
+						if (data == 'You have Successfully Registered.....') {
+							$("form")[0].reset();
+						}
+						alert(data);
+					});
+				}
+	});
 });

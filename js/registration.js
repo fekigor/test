d07903cd71ec9f488493a8a3cf3c1ec60c74f886
igorf <igorf@igorf-desktop.(none)>
2014-12-16 13:47:52 +0200

    Next step. Captcha validation.

--- js/registration.js
+++ js/registration.js
@@ -7,14 +7,14 @@ $(document).ready(function() {
                 });
 
 	$("#register").click(function() {
-	var fname = $("#fname").val();
-	var sname = $("#sname").val();
-	var email = $("#email").val();
-	var age = $("#age").val();
-	var sex = $("#sex").val();
-	var country = $("#country").val();
-	var city = $("#city").val();
-	var pcode = $("#pcode").val();
+		var fname = $("#fname").val();
+		var sname = $("#sname").val();
+		var email = $("#email").val();
+		var age = $("#age").val();
+		var sex = $("#sex").val();
+		var country = $("#country").val();
+		var city = $("#city").val();
+		var pcode = $("#pcode").val();
 	if (fname == '' || sname == '' || email == '' || age == '' || sex == '' || country == '' || city == '' || pcode == ''  ) {
 		alert("NB! Please fill all fields!");
 	} else 
@@ -23,21 +23,31 @@ $(document).ready(function() {
 				} else if ((sname.length) < 2) {
 					alert("Surname should atleast 2 character in length...!");
 				} else {
-				$.post("register.php", {
-					fname1: fname,
-					sname1: sname,
-					email1: email,
-					age1: age,
-					sex1: sex,
-					country1: country,
-					city1: city,
-					pcode1: pcode
-					}, function(data) {
-						if (data == 'You have Successfully Registered.....') {
-							$("form")[0].reset();
+					// Validating CAPTCHA with user input text.
+					var dataString = 'captcha=' + captcha;
+					$.ajax({
+						type: "POST",
+						url: "verify.php",
+						data: dataString,
+						success: function(html) {
+							alert(html);
 						}
-						alert(data);
-					});
+					});					
+					$.post("register.php", {
+						fname1: fname,
+						sname1: sname,
+						email1: email,
+						age1: age,
+						sex1: sex,
+						country1: country,
+						city1: city,
+						pcode1: pcode
+						}, function(data) {
+							if (data == 'You have Successfully Registered!') {
+								$("form")[0].reset();
+							}
+							alert(data);
+						});
 				}
 	});
 });
